88c82e63ff1208ada3e6fc6de5680bed57f41ef2
Lujine <lujineelfeky@gmail.com>
2019-11-28 17:53:42 +0200

    feat/turn item into a nested object

--- db/validations/storevalidations.js
+++ db/validations/storevalidations.js
@@ -1,37 +1,47 @@
-const joi = require('joi')
+const joi = require('joi');
+joi.objectId = require('joi-objectid')(joi)
+
+const createItemSchema = {
+  name: joi.string().required(),
+  price: joi.number().required(),
+  barcode: joi.number().min(100000000000).max(999999999999).required(),
+  discount: joi.number(),
+};
+
+const updateItemSchema = {
+  name: joi.string(),
+  price: joi.number(),
+  barcode: joi.number().min(100000000000).max(999999999999),
+  discount: joi.number(),
+};
 
 module.exports = {
-  createValidation: req => {
+  createItemSchema,
+  createValidation: (req) => {
     const createSchema = {
       name: joi.string().required(),
       address: joi.string().required(),
       phoneNumber: joi.number().min(1000000000).max(9999999999).required(),
       items: joi.array().items(
-          joi.object().keys({
-            name:joi.string().required(),
-            price:joi.number().required(),
-            barcode:joi.number().min(100000000000).max(999999999999).required(),
-            discount:joi.number()
-          }).required()
-      ).required()
-    }
-    return joi.validate(req, createSchema)
+        joi.object().keys(
+          createItemSchema,
+        ),
+      ).required(),
+    };
+    return joi.validate(req, createSchema);
   },
-
-  updateValidation: req => {
+  updateItemSchema,
+  updateValidation: (req) => {
     const updateSchema = {
-        name: joi.string(),
-        address: joi.string(),
-        phoneNumber: joi.number().min(1000000000).max(9999999999),
-        items: joi.array().items(
-            joi.object().keys({
-              name:joi.string(),
-              price:joi.number(),
-              barcode:joi.number().min(100000000000).max(999999999999),
-              discount:joi.number()
-            })
-        )
-      }
-    return joi.validate(req, updateSchema)
-  }
-}
+      name: joi.string(),
+      address: joi.string(),
+      phoneNumber: joi.number().min(1000000000).max(9999999999),
+      items: joi.array().items(
+        joi.object().keys(
+          updateItemSchema,
+        ),
+      ),
+    };
+    return joi.validate(req, updateSchema);
+  },
+};
